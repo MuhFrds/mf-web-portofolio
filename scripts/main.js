d2fb3abb450154b5d1936e50bb4c000f54fbc3d0
Muh Firdaus <muhammadfirdaus66538@gmail.com>
2023-08-01 16:48:51 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,16 @@
+// toggle and responsive navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
 // clear form before unload
 
 window.onbeforeunload = () => {
